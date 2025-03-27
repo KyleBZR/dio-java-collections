19169d7ba99a7be0053113e31482b2b074d8e163
KyleBZR <bzrpedro@gmail.com>
2025-03-27 14:13:36 -0300

    resolução de exercícios durante a aula

---
+++ src/listas/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,20 @@
+package listas.OperacoesBasicas;
+
+public class Tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao)
+    {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao()
+    {
+        return this.descricao;
+    }
+
+    public String toString()
+    {
+        return descricao; 
+    }
+}
